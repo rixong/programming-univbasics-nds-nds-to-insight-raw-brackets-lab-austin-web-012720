7c55a11331dcb306ed20473ebd09225c7f0bffcf
Rick Glascock <rixong@gmail.com>
2020-01-13 15:01:28 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,8 +5,9 @@ def directors_totals(nds)
   dirs_total = {}
   nds.each { |director| 
     ind_total = 0
-    director[:movies].each |movie| {
-		ind_total += movie[:worldwide_gross]}
+    director[:movies].each { |movie|
+		ind_total += movie[:worldwide_gross]
+    }
 	dirs_total[director[:name] => ind_total]
   }
   return 
