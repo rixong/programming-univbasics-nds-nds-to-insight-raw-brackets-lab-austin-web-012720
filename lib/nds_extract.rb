a3aa3456ea7ed594ee6db78aec400ecf88328a87
Rick Glascock <rixong@gmail.com>
2020-01-13 15:07:44 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,14 +1,14 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
-puts directors_database[0][:movies][0][:worldwide_gross]
+p directors_database[0][:movies][0][:worldwide_gross]
 
 def directors_totals(nds)
   dirs_total = {}
   nds.each { |director| 
     ind_total = 0
     director[:movies].each { |movie|
-		ind_total += movie[:worldwide_gross].to_i
+		ind_total += movie[:worldwide_gross]
     }
 	dirs_total[director[:name] => ind_total]
   }
